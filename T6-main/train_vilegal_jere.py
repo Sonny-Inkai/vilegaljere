674469b4ce6510f1b6d90615e062db2fcb554f0b
Sonny-Inkai <sonnyinkai@gmail.com>
2025-06-10 14:15:40 +0700

    init

--- T6-main/train_vilegal_jere.py
+++ T6-main/train_vilegal_jere.py
@@ -14,7 +14,7 @@ from transformers import AutoTokenizer
 
 # -----------------------------------------------------------------------------
 # -- CÔNG TẮC CHÍNH --
-finetune = True
+finetune = False
 # -----------------------------------------------------------------------------
 
 # --- Cấu hình chung ---
@@ -35,13 +35,13 @@ if finetune:
     
     # Siêu tham số cho fine-tuning
     learning_rate = 1e-4 # T5 fine-tuning thường hoạt động tốt hơn với LR cao hơn một chút
-    max_iters = 13000    # Số vòng lặp ít hơn
-    batch_size = 8       # Batch size có thể nhỏ hơn
-    gradient_accumulation_steps = 4
+    max_iters = 20    # Số vòng lặp ít hơn
+    batch_size = 32       # Batch size có thể nhỏ hơn
+    gradient_accumulation_steps = 2
     weight_decay = 0.001
-    eval_interval = 100
-    log_interval = 10
-    eval_iters = 20
+    eval_interval = 1#100
+    log_interval = 1#10
+    eval_iters = 1#20
     
 else:
     # --- CẤU HÌNH CHO PRE-TRAINING ---
@@ -51,13 +51,13 @@ else:
     
     # Siêu tham số cho pre-training
     learning_rate = 3e-4  # Good for T5-small
-    max_iters = 10000     # Very small for testing
-    batch_size = 16      # Even smaller for T4 memory constraints
+    max_iters = 10 #10000     # Very small for testing
+    batch_size = 32      # Even smaller for T4 memory constraints
     gradient_accumulation_steps = 4   # Reduced to avoid memory issues
     weight_decay = 1e-3
-    eval_interval = 500  # More frequent eval for shorter training
-    log_interval = 10   # More frequent logging
-    eval_iters = 200     # Fewer eval iterations to save time
+    eval_interval = 1#500  # More frequent eval for shorter training
+    log_interval = 1   # More frequent logging
+    eval_iters = 1#200     # Fewer eval iterations to save time
     
 # wandb logging
 wandb_log = True    # Enable for better tracking
